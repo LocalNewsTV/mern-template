a9218899064dbc8e3c84fdb4cad55ddde455f2cc
Matthew Logan <62873746+LocalNewsTV@users.noreply.github.com>
2024-01-29 21:16:38 -0800

    Update create-db.js

--- mongoSeed/init/create-db.js
+++ mongoSeed/init/create-db.js
@@ -1,7 +1,6 @@
 /**
  * @desc  Seed Data for Development MongoDB Database, contains: 
  *  - initial user account for database access
- *  - Sample set of location data for Hospitals, Courts, ServiceBC, ICBC, and BC Housing 
  *  - Set of 'UpdateDate' data for frontend testing
  */
 
